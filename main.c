c188fdae3fd2aecc4d7e58df71186a35af71c8cc
Maximilian Stocker <max.stocker@gmx.de>
2022-11-08 20:32:13 +0100

    Übung 5 gelöst

--- main.c
+++ main.c
@@ -63,61 +63,94 @@ void siedlerWuerfel(){
 	}
 }
 
+void nickelback() {
+	int muenze_e2 = 0;
+	int muenze_e1 = 0;
+	int muenze_c50 = 0;
+	int muenze_c20 = 0;
+	int muenze_c10 = 0;
+	int muenze_c5 = 0;
+	int muenze_c2 = 0;
+	int muenze_c1 = 0;
+	float eingabe = 0.00;
 
-void muenzAutomat()
-{
-	float input = 0;
-	printf("Eingabe: ");
-	scanf("%6f", &input);
-	input *= 100;//konvertierung in cents
-		     
-	int maxCoins = 100;
-	int coins[8] = {200, 100, 50, 20, 10, 5, 2, 1};
-	int output[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-	int checkSolution = 0;
-	int coinCount = 0;
-	int inputBackup = input;
-
-	if (input <= 20000)
-	{
-
-		for (int i = 0; i < 8; i++)
-		{
-
-			int counter = 0;
-
-			while ( counter < maxCoins && (input - coins[i] >= 0) )
-			{
-
-				input -= coins[i];
-
-				output[i]++;
-
-				counter++;
-			}
+	printf("Bitte geben Sie einen Betrag bis 200€ mit maximal zwei Nachkommastellen ein. \n");
+	while (eingabe <= 0.00 || eingabe > 200.00) {
+		printf("Ihre Eingabe: ");
+		scanf("%6f", &eingabe);
+		if (eingabe <= 0.00 || eingabe > 200.00) {
+			printf("Kein korrekter Betrag eingegeben. Bitte noch einmal wiederhohlen. \n");
 		}
-
-		for (int i = 0; i < 8; i++)
-		{
-			printf("Ausgabe: %3.2f: %d \n", (coins[i] / 100.0), output[i]);
-			checkSolution += output[i] * coins[i];
-			coinCount += output[i];
-		}
-
-		if (checkSolution != inputBackup)
-		{
-			printf("Ende: %d != %d \n", checkSolution, inputBackup);
-		}
-
-		printf("ENDE mit %d Muenzen! \n", coinCount);
-
-	} else {
-
-		printf("Geldbetrag ist zu Groß! \n");
-
 	}
 
+
+	int eingabe_cent = eingabe * 100;
+	// printf("%i", eingabe_cent);
+
+	while (eingabe_cent > 0) {
+		if (eingabe_cent >= 200 && muenze_e2 < 100) {
+			// 2€ Stück verwenden
+			muenze_e2++;
+			eingabe_cent -= 200;
+		} else if (eingabe_cent >= 100 && muenze_e1 < 100) {
+			// 1€ Stück verwenden
+			muenze_e1++;
+			eingabe_cent -= 100;
+		} else if (eingabe_cent >= 50 && muenze_c50 < 100) {
+			// 50Cent Stück verwenden
+			muenze_c50++;
+			eingabe_cent -= 50;
+		} else if (eingabe_cent >= 20 && muenze_c20 < 100) {
+			// 20Cent Stück verwenden
+			muenze_c20++;
+			eingabe_cent -= 20;
+		} else if (eingabe_cent >= 10 && muenze_c10 < 100) {
+			// 10Cent Stück verwenden
+			muenze_c10++;
+			eingabe_cent -= 10;
+		} else if (eingabe_cent >= 5 && muenze_c5 < 100) {
+			// 5Cent Stück verwenden
+			muenze_c5++;
+			eingabe_cent -= 5;
+		} else if (eingabe_cent >= 2 && muenze_c2 < 100) {
+			// 2Cent Stück verwenden
+			muenze_c2++;
+			eingabe_cent -= 2;
+		} else if (eingabe_cent >= 1 && muenze_c1 < 1) {
+			// 1Cent Stück verwenden
+			muenze_c1++;
+			eingabe_cent -= 1;
+		}
+	}
+	printf("Große Münzen: ");
+	if (muenze_e2 > 0) {
+		printf("%i x 2 Euro, ", muenze_e2);
+	}
+	if (muenze_e1 > 0) {
+		printf("%i x 1 Euro, ", muenze_e1);
+	}
+	if (muenze_c50 > 0) {
+		printf("%i x 50 Cent, ", muenze_c50);
+	}
+	if (muenze_c20 > 0) {
+		printf("%i x 20 Cent, ", muenze_c20);
+	}
+	if (muenze_c10 > 0) {
+		printf("%i x 10 Cent, ", muenze_c10);
+	}
+	if (muenze_c5 > 0) {
+		printf("%i x 5 Cent, ", muenze_c5);
+	}
+	if (muenze_c2 > 0) {
+		printf("%i x 2 Cent, ", muenze_c2);
+	}
+	if (muenze_c1 > 0) {
+		printf("%i x 1 Cent, ", muenze_c1);
+	}
+	printf("\n");
 }
+
+
 int main()
 {
 	
